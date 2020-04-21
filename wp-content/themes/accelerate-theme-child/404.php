b38a519473bbb4713bdce2c7e60230b8f76d2755
tokada <theodoreokada@gmail.com>
2020-04-21 20:14:42 +0900

    gag 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+<div class="p404-wrapper">
+  <div class="p404-content">
+
+    <img class="p404-img" src="https://www.publicdomainpictures.net/pictures/40000/velka/sad-dog.jpg" alt="sad dog because of 404" />
+
+    <div class="p404-txt">
+      <h1>Well this is ruff...</h1>
+      <h2>She protecc. She attacc.<br />But mostly importantly she wants to go <a href="https://ttostage.dreamhosters.com/">bacc.</a></h2>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<?php get_footer(); ?>
